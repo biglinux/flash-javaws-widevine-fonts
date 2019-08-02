94a2889767e8636ea4682c2a9f2dde9fe0c4b6ba
Bruno Goncalves <bigbruno@gmail.com>
2019-08-02 04:54:47 -0300

    ok

---
+++ flash-javaws-widevine-fonts/usr/share/bigbashview/bcc/apps/flash-javaws-widevine-fonts/apps-manage.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+windowID="$(xprop -root '\t$0' _NET_ACTIVE_WINDOW | cut -f 2)"
+    
+if [ "$1" = "install-font" ]; then
+    echo "ttf-mscorefonts-installer" | sed 's/$/\ /;s/--install //' | sed 's/\ /\ install\r\n/g' > /tmp/bigsynaptic$$
+    synaptic --parent-window-id "$windowID" --set-selections --non-interactive -o Synaptic::closeZvt=true --hide-main-window < /tmp/bigsynaptic$$
+fi
+
+
+if [ "$1" = "remove-font" ]; then
+    echo "ttf-mscorefonts-installer" | sed 's/$/\ /;s/--remove //' | sed 's/\ /\ uninstall\r\n/g' > /tmp/bigsynaptic$$
+    synaptic --parent-window-id "$windowID" --set-selections --non-interactive -o Synaptic::closeZvt=true --hide-main-window < /tmp/bigsynaptic$$
+fi
+
+if [ "$1" = "install-flash" ]; then
+    echo "adobe-flashplugin" | sed 's/$/\ /;s/--install //' | sed 's/\ /\ install\r\n/g' > /tmp/bigsynaptic$$
+    synaptic --parent-window-id "$windowID" --set-selections --non-interactive -o Synaptic::closeZvt=true --hide-main-window < /tmp/bigsynaptic$$
+fi
+
+
+if [ "$1" = "remove-flash" ]; then
+    echo "adobe-flashplugin" | sed 's/$/\ /;s/--remove //' | sed 's/\ /\ uninstall\r\n/g' > /tmp/bigsynaptic$$
+    synaptic --parent-window-id "$windowID" --set-selections --non-interactive -o Synaptic::closeZvt=true --hide-main-window < /tmp/bigsynaptic$$
+fi
+
+if [ "$1" = "remove-widevine" ]; then
+    rm -f /usr/lib/chromium-browser/libwidevinecdm.so
+fi
+
+if [ "$1" = "config-javaws" ]; then
+    itweb-settings
+fi
+
+if [ "$1" = "install-javaws" ]; then
+    echo "icedtea-netx" | sed 's/$/\ /;s/--install //' | sed 's/\ /\ install\r\n/g' > /tmp/bigsynaptic$$
+    synaptic --parent-window-id "$windowID" --set-selections --non-interactive -o Synaptic::closeZvt=true --hide-main-window < /tmp/bigsynaptic$$
+fi
